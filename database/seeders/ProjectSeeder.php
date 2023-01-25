20e72b697c314c5c00f4d3033d0d1733992571c8
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-25 14:42:02 +0100

    aggiunge seed del db tramite faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,13 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $new_project = new Project();
+        $new_project->title = $faker->sentence();
+        $new_project->description = $faker->text();
+        $new_project->client_name = $faker->firstName();
+        $new_project->slug = Str::slug($new_project->title, '-');
+        $new_project->save();
     }
 }
